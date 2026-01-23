ad9febf71aad0a8ed7d3a673795c780b29290557
Cristian Castillo <inbox@cris.mx>
2026-01-22 23:44:59 -0600

    Update class-imgpro-cdn-api.php

--- includes/class-imgpro-cdn-api.php
+++ includes/class-imgpro-cdn-api.php
@@ -945,7 +945,7 @@ class ImgPro_CDN_API {
         return [
             'bandwidth_used'   => $usage['bandwidth']['used_bytes'] ?? 0,
             'bandwidth_limit'  => $usage['bandwidth']['limit_bytes'] ?? 0,
-            'cache_used'       => $usage['cache']['used_bytes'] ?? 0,
+            'cache_used'       => 0, // Cache usage tracking deprecated
             'cache_limit'      => $usage['cache']['limit_bytes'] ?? 0,
             'cache_hits'       => $usage['cache_hits'] ?? 0,
             'cache_misses'     => $usage['cache_misses'] ?? 0,
